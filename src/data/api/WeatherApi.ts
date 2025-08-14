0246bcb40b8db0fef5eed690a2b3408e13d7c1ec
Guillaume Belouin <gbelouin@protonmail.com>
2025-08-14 10:38:18 +0200

    Create constant for open meteo URL

--- src/data/api/WeatherApi.ts
+++ src/data/api/WeatherApi.ts
@@ -1,6 +1,8 @@
 import type WeatherApiResponse from "./model/WeatherApiResponse.ts";
 
+const openMeteoURL = "https://api.open-meteo.com"
+
 export default async function getCurrentWeather(): Promise<WeatherApiResponse> {
-  const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
+  const response = await fetch(openMeteoURL + "/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
   return await response.json()
 }
